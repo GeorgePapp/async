ba46e9c13c24712baff073d8f61fd31b77bf4b1b
George pappass <papgeo43@gmail.com>
2020-02-03 11:16:47 +0200

    catches errors

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -181,7 +181,7 @@ function getUserData(inputValue) {
       });
     });
   }).catch(function (Error) {
-    return console.log('error');
+    return console.log("".concat(Error));
   });
 }
 
@@ -236,7 +236,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51782" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52955" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
